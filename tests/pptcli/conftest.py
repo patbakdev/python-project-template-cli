fe7d749e708a62205480a0a8c7795353be217540
Patrick Baker <patrick@patrickbaker.dev>
2021-04-01 23:19:40 -0700

    WIP

--- tests/pptcli/conftest.py
+++ tests/pptcli/conftest.py
@@ -16,27 +16,4 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from setuptools import setup, find_packages
-
-setup(
-    name="pptcli",
-    version="0.1.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    entry_points={"console_scripts": ["pptcli = pptcli.__main__:main"]},
-    install_requires=[
-        "click==7.1.2",  # TODO
-        "click-log==0.3.2",  # TODO
-        "colorama==0.4.4",  # BSD License (3 clause)
-    ],
-    # devtest_requires=[
-    #     'black==20.8b1', # TODO
-    #     'isort==5.8.0', # TODO
-    #     'flake8==2.3.1', # TODO
-    #     'pylint==2.7.4', # GPLv2
-    #     'mypy==0.812', # TODO
-    #     'bandit==1.7.0', # TODO
-    #     'pytest==6.2.2', # MIT
-    #     'SNAPSHOT
-    # ],
-)
+"""Test Configuration for Project Template for Python Command Line Tools: Command Line Interface"""
